ebc570331b097ec81f30a0650a2f16ba86c2f92b
New <New@domain.ru>
2023-03-06 06:44:15 +0500

    Изменения в примере №47

--- Exemple47/Program.cs
+++ Exemple47/Program.cs
@@ -4,34 +4,32 @@
     string value = Console.ReadLine();
     int index = Convert.ToInt32(value);
     return index;
-}    
+}
 
-void GetArray(double[,] array, int indexStr, int indexCol)
+void GetArray(double[,] array)
 {
-    for(int i = 0; i < indexStr; i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for(int j = 0; j < indexCol; j++)
-        array[i, j] = new Random().Next(2, 10);
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(2, 10);
     }
 }
-// Выведем массив для проверки 
-void PrintArray(double[,] array, int indexStr, int indexCol)
+
+// Выведем массив для проверки
+void PrintArray(double[,] array)
 {
-    for(int i = 0; i < indexStr; i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for(int j = 0; j < indexCol; j++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
             Console.Write($"{array[i, j]}  ");
         }
         Console.WriteLine();
     }
-    
 }
-
 
 int indexStr = GetIndexArray("Введите количество строк  ");
 int indexCol = GetIndexArray("Введите количество столбцов  ");
 double[,] array = new double[indexStr, indexCol];
-GetArray(array, indexStr, indexCol);
-PrintArray(array, indexStr, indexCol);
-
+GetArray(array);
+PrintArray(array);
